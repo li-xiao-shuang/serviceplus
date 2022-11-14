32b396d98554955ed0eaeacb7f106c3af3a46910
lixiaoshuang <644968328@qq.com>
2022-11-14 23:50:39 +0800

    定义broker、client模块

---
+++ broker/src/main/java/org/serviceplus/broker/BrokerApplication.java
@@ -0,0 +1,18 @@
+package org.serviceplus.broker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * broker 启动类
+ *
+ * @author lixiaoshuang
+ */
+@SpringBootApplication
+public class BrokerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrokerApplication.class, args);
+    }
+
+}
---
+++ broker/src/test/java/org/serviceplus/broker/BrokerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.serviceplus.broker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrokerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
